64d14ab2017e007116f3d268804e33e249fdc272
ZunFox <zunny932@gmail.com>
2026-04-03 12:54:24 +0700

    ✏️ Bài 49: exercises.py

---
+++ Python/Lesson0049/exercises.py
@@ -0,0 +1,27 @@
+import inspect
+from typing import Any, Callable, List, Union
+
+# Bài 1: Viết hàm list_public_methods(cls)
+def list_public_methods(cls):
+    """In ra tất cả các phương thức public (không bắt đầu bằng '_') của một lớp"""
+    pass
+
+# Bài 2: Viết decorator @log_calls
+def log_calls(func):
+    """Ghi log mỗi khi hàm được gọi, bao gồm tên hàm và tham số"""
+    pass
+
+# Bài 3: Viết hàm analyze_function(func)
+def analyze_function(func):
+    """In ra chi tiết về một hàm: tên, tham số, kiểu, giá trị mặc định"""
+    pass
+
+# Bài 4: Viết hàm find_classes_in_module(module)
+def find_classes_in_module(module):
+    """Trả về danh sách các lớp trong một module"""
+    pass
+
+# Bài 5: Viết hàm validate_call(func, **kwargs)
+def validate_call(func, **kwargs):
+    """Kiểm tra xem kwargs có phù hợp với chữ ký của func không"""
+    pass
