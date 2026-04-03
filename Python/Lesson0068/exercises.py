c27a252c8a8521877bc63d0e3b089ad56abf5637
ZunFox <zunny932@gmail.com>
2026-04-03 12:58:13 +0700

    ✏️ Bài 68: exercises.py

---
+++ Python/Lesson0068/exercises.py
@@ -0,0 +1,57 @@
+import inspect
+import time
+
+# Bài tập 1: Viết hàm in ra tên và giá trị các tham số trong hàm gọi nó
+
+def in_tham_so():
+    # TODO: Hoàn thiện hàm này
+    pass
+
+
+def ham_thu(a, b, c="mac_dinh"):
+    in_tham_so()
+
+
+# Bài tập 2: Viết hàm in ra tất cả các lớp trong một module
+
+def liet_ke_lop(module):
+    # TODO: Hoàn thiện hàm này
+    pass
+
+
+class LopA:
+    pass
+
+class LopB:
+    pass
+
+
+# Bài tập 3: Tạo decorator ghi log tên hàm và thời gian thực thi
+
+def thoi_gian_thuc_thi(func):
+    # TODO: Hoàn thiện decorator
+    pass
+
+
+# Bài tập 4: Liệt kê các phương thức public của một lớp
+
+def liet_ke_phuong_thuc_public(cls):
+    # TODO: Hoàn thiện hàm
+    pass
+
+class MauLop:
+    def cong(self, a, b):
+        return a + b
+
+    def _rieng_tu(self):
+        pass
+
+    def __duoc_bao_ve(self):
+        pass
+
+
+# Bài tập 5: Tìm các hàm bắt đầu bằng 'test_' trong module hiện tại
+
+def tim_ham_test(module):
+    # TODO: Hoàn thiện hàm
+    pass
