4756ac0e552a4d602af479e50380b5205aee4fd8
ZunFox <zunny932@gmail.com>
2026-04-03 13:03:12 +0700

    ✏️ Bài 98: exercises.py

---
+++ Python/Lesson0098/exercises.py
@@ -0,0 +1,21 @@
+import inspect
+
+def print_class_info(cls):
+    """Bài 1: In ra tất cả phương thức và thuộc tính của một lớp."""
+    pass
+
+def debug_calls(func):
+    """Bài 2: Decorator in ra tên hàm, tham số và giá trị trả về."""
+    pass
+
+def get_caller_info():
+    """Bài 3: In ra tên hàm gọi nó và tên file."""
+    pass
+
+def require_kwargs_only(func):
+    """Bài 4: Decorator yêu cầu tất cả tham số phải là keyword arguments."""
+    pass
+
+def list_functions_in_module(module):
+    """Bài 5: Liệt kê tất cả hàm trong module cùng số lượng tham số."""
+    pass
