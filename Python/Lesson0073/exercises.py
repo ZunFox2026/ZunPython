53534629efb14e32464aad7afe5359dd8b00db2d
ZunFox <zunny932@gmail.com>
2026-04-03 12:59:01 +0700

    ✏️ Bài 73: exercises.py

---
+++ Python/Lesson0073/exercises.py
@@ -0,0 +1,21 @@
+import inspect
+
+def print_class_info(cls):
+    """Bài 1: In ra tất cả phương thức và thuộc tính của một lớp"""
+    pass
+
+def log_call(func):
+    """Bài 2: Viết decorator @log_call ghi lại tên hàm, tham số và giá trị trả về"""
+    pass
+
+def find_functions_in_file(module, line_threshold):
+    """Bài 3: Tìm các hàm có số dòng mã nguồn > line_threshold"""
+    pass
+
+def who_called_me():
+    """Bài 4: In ra tên hàm đã gọi hàm hiện tại"""
+    pass
+
+def validate_types(func):
+    """Bài 5: Kiểm tra kiểu tham số theo annotation. Trả về wrapper kiểm tra kiểu"""
+    pass
